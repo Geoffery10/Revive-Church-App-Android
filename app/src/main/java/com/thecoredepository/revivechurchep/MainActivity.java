ccdc081d87ab0085fff448d18ff9c252e69e0e29
Geoffery Powell <powellgeoffery@gmail.com>
2020-03-28 12:43:02 -0500

    Added No Internet Splash Screen

--- app/src/main/java/com/thecoredepository/revivechurchep/MainActivity.java
+++ app/src/main/java/com/thecoredepository/revivechurchep/MainActivity.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -37,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -49,6 +53,14 @@ public class MainActivity extends AppCompatActivity {
             public void onPageFinished(WebView view, final String url) {
                 splashView.setVisibility(View.GONE);
                 webView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error){
+                splashView.setVisibility(View.VISIBLE);
+                webView.setVisibility(View.GONE);
+                TextView txtTextSplash = findViewById(R.id.txtTextSplash);
+                txtTextSplash.setText("No Internet... Please try again.");
             }
         });
 
