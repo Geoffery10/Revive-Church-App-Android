c9159b9cc8284cc7bb341f55f89fd7719a3e71f5
Geoffery Powell <powellgeoffery@gmail.com>
2020-03-28 13:11:44 -0500

    Added Open in Facebook for one of the Facebook Links

--- app/src/main/java/com/thecoredepository/revivechurchep/MainActivity.java
+++ app/src/main/java/com/thecoredepository/revivechurchep/MainActivity.java
@@ -2,7 +2,11 @@ package com.thecoredepository.revivechurchep;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebResourceError;
@@ -18,6 +22,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 public class MainActivity extends AppCompatActivity {
 
     Activity activity;
+    Context context;
     private WebView webView;
 
     @SuppressLint("NewApi")
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         activity = this;
+        context = this;
 
         final ConstraintLayout splashView = findViewById(R.id.splashView);
         splashView.setVisibility(View.VISIBLE);
@@ -44,9 +50,19 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-
-                return true;
+                Log.d("URL", url);
+                if (url.equals("https://m.facebook.com/EPReviveChurch/?ref=br_rs")) //check if that's a url you want to load internally
+                {
+                    Log.d("URL", "URL = Facebook request");
+                    Intent fbIntent = getOpenFacebookIntent(context);
+                    startActivity(fbIntent);
+                    return false;
+                }
+                else
+                {
+                    view.loadUrl(url);
+                    return true;
+                }
             }
 
             @Override
@@ -82,5 +98,20 @@ public class MainActivity extends AppCompatActivity {
 
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    public static Intent getOpenFacebookIntent(Context context)
+    {
+        try
+        {
+            Log.d("URL", "Trying you open app");
+            context.getPackageManager().getPackageInfo("com.facebook.katana",0);
+            Log.d("URL", "Opened in Facebook App");
+            return new Intent(Intent.ACTION_VIEW,Uri.parse("fb://page/1093885720794176"));
+        }catch (Exception e)
+        {
+            Log.d("URL", "App loading failed. Opening URL.");
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/EPReviveChurch"));
+        }
     }
 }
